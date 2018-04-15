979aa64fa50242ef07f9ad2da90428d8f285aa22
artyom mirolyubov <me.photo.work@outlook.com>
2018-04-15 15:04:12 +0300

    update: add mock render f

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,7 +1,16 @@
 import { gameTypes } from '../actionTypes'
 
+const mockRender = ({x, y}) => {
+  if (y > 3 && y < 10) {
+    if (x === 5) return null
+    if (x === y || x === y + 1 || x === y - 2 || x === y - 3) return 'white'
+  }
+  return null
+}
+
 const initialState = {
-  matrix: new Array(15).fill(null).map(() => new Array(15).fill(null).map(() => Math.random() < 0.2 ? 'white' : Math.random() < 0.4 ? 'black' : null)),
+  // matrix: new Array(15).fill(null).map(() => new Array(15).fill(null).map(() => Math.random() < 0.2 ? 'white' : Math.random() < 0.4 ? 'black' : null)),
+  matrix: new Array(15).fill(null).map((i, key) => new Array(15).fill(null).map((y, senondKey) => mockRender({y: key, x: senondKey}))),
   role: 'black'
 }
 
